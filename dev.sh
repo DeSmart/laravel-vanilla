a66872462f60e3940f9170949ba20416dbfe5b08
Kamil Fojuth <fen@desmart.com>
2017-01-28 23:58:16 +0100

    Add some aliases to dev.sh

--- dev.sh
+++ dev.sh
@@ -1,2 +1,5 @@
 alias docker-init='eval $(docker-machine env default || docker-machine start default && docker-machine env default)'
 alias dc='docker-compose -f docker-compose.local.yml'
+alias drun='docker exec -it $(docker ps -qf "name=.*(php|app).*")'
+alias unit='drun vendor/bin/phpunit'
+alias un='unit'
