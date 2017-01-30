b2c1cd44a4b987821efff63570f77ecfcb03cadf
Kamil Fojuth <fen@desmart.com>
2017-01-30 21:25:43 +0100

    Change phpunit exec command to use docker-compose instead of docker

--- dev.sh
+++ dev.sh
@@ -1,5 +1,4 @@
 alias docker-init='eval $(docker-machine env default || docker-machine start default && docker-machine env default)'
 alias dc='docker-compose -f docker-compose.local.yml'
-alias drun='docker exec -it $(docker ps -qf "name=.*(php|app).*")'
-alias unit='drun vendor/bin/phpunit'
+alias unit='dc run --rm php vendor/bin/phpunit --verbose'
 alias un='unit'
